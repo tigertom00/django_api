4cc466a57dc0588d505fa7c1eb86efd12a54c41a
Tomas <tigertom00@gmail.com>
2021-03-29 20:10:40 +0200

    initial

---
+++ api/settings/dev.py
@@ -0,0 +1,78 @@
+from .base import *
+
+DEBUG = True
+
+SECRET_KEY = config('SECRET_KEY')
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+    'api.apps.testing',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+# * Paths
+VENV_PATH = Path(BASE_DIR).joinpath('django_web/static')
+STATIC_URL = '/django_web/static/'
+MEDIA_URL = '/django_web/media/'
+STATICFILES_DIRS = [Path(VENV_PATH) / 'static_in_env']
+STATIC_ROOT = Path(VENV_PATH) / 'static_root'
+MEDIA_ROOT = Path(VENV_PATH) / 'media_root'
+
+# * Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': Path(BASE_DIR) / 'db.sqlite3',
+    }
+}
+
+# * Email
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = Path(VENV_PATH) / 'email'
+
+# * Cors Headers
+CORS_ALLOWED_ORIGINS = [
+    "https://nxfs.no",
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://localhost:5500"
+]
+
+# * Debug Toolbar
+INTERNAL_IPS = [
+    # ...
+    '127.0.0.1',
+    # ...
+]
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.history.HistoryPanel',
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+]
+
+
+def show_toolbar(request):
+    return True
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'SHOW_TOOLBAR_CALLBACK': show_toolbar
+}
---
+++ api/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.conf import settings
+from django.urls import path, include
+
+
+from api.settings.base import BASE_DIR
+from api.settings.dev import VENV_PATH
+import sys
+print(BASE_DIR, file=sys.stderr)
+print(VENV_PATH, file=sys.stderr)
+
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+    path('home/', include('api.apps.home.urls')),
+    path('api/users/', include('api.apps.users.urls')),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+        path('api/testing', include('api.apps.testing.urls')),
+    ]
